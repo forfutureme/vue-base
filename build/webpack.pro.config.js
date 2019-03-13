647926dd81ea3326420dd7a16a5162d6819a2183
胡伟健 <598307691@qq.com>
2019-03-13 18:58:54 +0800

    优化Webpack配置

--- build/webpack.pro.config.js
+++ build/webpack.pro.config.js
@@ -6,7 +6,9 @@ const webpack = require('webpack')
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 let projectRoot = process.cwd()
 const baseConfig = require('./webpack.base.config')
 let config = Object.assign({}, baseConfig, {
@@ -37,18 +39,59 @@ let config = Object.assign({}, baseConfig, {
     //   disable: false,
     //   allChunks: true
     // })
+    // 使用css提取插件
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: 'css/[name]_[hash:8].css',
       chunkFilename: 'css/[id]_[hash:8].css'
+    }),
+    // 开启gzip压缩
+    new CompressionWebpackPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp('\\.(js|css)$'),
+      threshold: 10240,
+      minRatio: 0.8
     })
-
   ]),
   optimization: {
     minimizer: [
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({
+        assetNameRegExp: /\.optimize\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { safe: true, discardComments: { removeAll: true } },
+        canPrint: true
+      })
+    ],
+    minimize: true,
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000, //模块大于30k会被抽离到公共模块
+      minChunks: 1, //模块出现1次就会被抽离到公共模块
+      maxAsyncRequests: 5, //异步模块，一次最多只能被加载5个
+      maxInitialRequests: 3, //入口模块最多只能加载3个
+      name: true,
+      cacheGroups: {
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
+    },
+    runtimeChunk: {
+      name: 'runtime'
+    }
   }
 })
 // console.log(JSON.stringify(config))
--- src/app.js
+++ src/app.js
@@ -15,20 +15,20 @@ import './assets/stylus/style.styl'
 import amfeFlexible from 'amfe-flexible' //rem设置js
 import es6Promise from 'es6-promise'
 
-import {
-  Swipe,
-  SwipeItem
-} from 'mint-ui'
-import 'mint-ui/lib/swipe/style.css'
-import 'mint-ui/lib/swipe-item/style.css'
+// import {
+//   Swipe,
+//   SwipeItem
+// } from 'mint-ui'
+// import 'mint-ui/lib/swipe/style.css'
+// import 'mint-ui/lib/swipe-item/style.css'
 
-import ECharts from 'vue-echarts'
+// import ECharts from 'vue-echarts'
 
 // import method from './tools/method'
 // import JTBridge from './tools/bridge'
-Vue.component('v-chart', ECharts)
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
+// Vue.component('v-chart', ECharts)
+// Vue.component(Swipe.name, Swipe);
+// Vue.component(SwipeItem.name, SwipeItem);
 // require('imports-loader?this=>window!modernizr/modernizr.js') // 引入浏览器特性检查工具
 const FastClick = require('fastclick')
 // 注册工具方法
